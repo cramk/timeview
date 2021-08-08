fb9f3ec37dd490e70e8f7ba877ecaa4caeeca541
cramk <unknown@unknown>
2021-08-07 22:45:00 -0400

    Fix lint

--- src/views/WeeksView.js
+++ src/views/WeeksView.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { useEffect, useState } from "react";
+
 import Display from "../components/Display";
 
 function App() {
-  const [] = useState(null);
   const EXPECTED_AGE = 100; // 100 years
   const WEEKS_IN_LIFE = EXPECTED_AGE * 52;
-  useEffect(() => {}, []);
+
   return (
     <main>
       <caption>Weeks View</caption>
