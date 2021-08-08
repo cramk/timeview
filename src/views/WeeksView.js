b955675dd0c8c67c453bff8b1f3e20a8f11886b4
cramk <unknown@unknown>
2021-08-07 21:59:00 -0400

    Base start

--- src/views/WeeksView.js
+++ src/views/WeeksView.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import "./App.css";
-import WeeksView from "./views/WeeksView";
 
 function App() {
   const [] = useState(null);
   useEffect(() => {}, []);
   return (
     <main>
-      <h1>Timeview</h1>
-      <WeeksView />
+      <h2>Weeks View</h2>
+      boxes here
     </main>
   );
 }
